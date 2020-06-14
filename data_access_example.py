919fb0877ce980c834cfe0c1648ddfa902f2d5b6
A. Au <affaau@gmail.com>
2020-06-14 12:47:32 +0800

    data_access_example.py improve format

--- data_access_example.py
+++ data_access_example.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 
+
 singersdf = pd.DataFrame()
-singersdf['singer'] = pd.Series(['The Rolling Stones','Beatles', 'Guns N\' Roses', 'Metallic'])
-singersdf['song'] = pd.Series(['Satisfaction', 'Let It Be', 'Don\'t Cry','Nothing Else Matters'])
+singersdf['singer'] = pd.Series([
+                        'The Rolling Stones',
+                        'Beatles',
+                        'Guns N\' Roses',
+                        'Metallic'])
+singersdf['song'] = pd.Series([
+                        'Satisfaction',
+                        'Let It Be',
+                        'Don\'t Cry',
+                        'Nothing Else Matters'])
 print(singersdf)
 singersdf.to_csv('data_acces.csv')
 
